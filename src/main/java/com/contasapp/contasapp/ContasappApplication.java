646de7337f6b6f96547cb31eece81c0a34a97180
Adriano <adriangonc@yahoo.com.br>
2019-06-24 20:18:14 -0300

    first commit

---
+++ src/main/java/com/contasapp/contasapp/ContasappApplication.java
@@ -0,0 +1,14 @@
+package com.contasapp.contasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasappApplication.class, args);
+	}
+
+	
+}
---
+++ src/test/java/com/contasapp/contasapp/ContasappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.contasapp.contasapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContasappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
