1fc54d140547955d9a53079eff91b8b85cf82f68
ADRIANO GONCALVES DE SOUZA <UZ96445@usiminas.com>
2019-08-07 16:44:37 -0300

    criação de banco de desenvolvimento h2

--- src/main/java/com/contasapp/DataConfigurationH2.java
+++ src/main/java/com/contasapp/DataConfigurationH2.java
@@ -9,17 +9,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
-public class DataConfigurationDev {
+@Profile("devh2")
+public class DataConfigurationH2 {
 	
 	@Bean
     public BasicDataSource dataSource() throws URISyntaxException {
-       String dbUrl = "jdbc:postgresql://localhost:5432/postgres"; 
+       String dbUrl = "jdbc:h2:file:~/h2db"; 
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername("postgres");
-        basicDataSource.setPassword("desenv");
+        basicDataSource.setUsername("h2sa");
+        basicDataSource.setPassword("admin");
 
         return basicDataSource;
     }
